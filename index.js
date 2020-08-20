7e667bcaa4fa7e43d3802168f3f4ce37a1eb610a
Sunil kumar M <sunil.kumar@piktorlabs.com>
2020-08-20 17:20:02 +0530

    initial setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const userRoutes = require("./routes/users");
+const tasksRoutes = require("./routes/tasks");
+
+require("./db/mongoose");
+
+const app = express();
+app.use(express.json());
+app.use(userRoutes);
+app.use(tasksRoutes);
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log("Server is up on port " + port);
+});
